6e2e76968937dcca67f0ac864e9ca275e7cb123c
Emily <emilylaetitiawan@gmail.com>
2023-11-09 17:04:35 -0500

    add comment model

--- P2/petpal/comments/models.py
+++ P2/petpal/comments/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from accounts.models import PetUser
 
 # Create your models here.
+class Comment(models.Model):
+    commenter = models.ForeignKey(PetUser, on_delete=models.CASCADE)
+    comment = models.TextField()
+    rating = models.IntegerField(null=True, blank=True)
+    replied = models.ForeignKey(PetUser, null=True)
