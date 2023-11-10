21e2c98dc68c921414859a075d9cfc9a256468f5
Emily <emilylaetitiawan@gmail.com>
2023-11-10 00:07:56 -0500

    comment model

--- P2/petpal/comments/models.py
+++ P2/petpal/comments/models.py
@@ -3,7 +3,7 @@ from accounts.models import PetUser
 
 # Create your models here.
 class Comment(models.Model):
-    commenter = models.ForeignKey(PetUser, on_delete=models.CASCADE)
+    commenter = models.ForeignKey(PetUser, on_delete=models.CASCADE, related_name='commenter')
     comment = models.TextField()
     rating = models.IntegerField(null=True, blank=True)
-    replied = models.ForeignKey(PetUser, null=True, on_delete=models.CASCADE)
+    replied = models.ForeignKey(PetUser, null=True, on_delete=models.CASCADE, related_name='replied')
