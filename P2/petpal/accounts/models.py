abdf8d81dd134881d47a55bd4926093d2b670f33
xuyangan <annaxu44@gmail.com>
2023-11-09 23:49:54 -0500

    pet_listing implementations and model

--- P2/petpal/accounts/models.py
+++ P2/petpal/accounts/models.py
@@ -3,5 +3,10 @@ from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
 class PetUser(AbstractUser):
-    shelter_name = models.CharField(null=True, blank=True)
-    REQUIRED_FIELDS = ['username', 'email']
+    shelter_name = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+    
--- P2/petpal/pet_listings/models.py
+++ P2/petpal/pet_listings/models.py
@@ -1,3 +1,63 @@
 from django.db import models
+from accounts.models import PetUser
+# from accounts.models import Shelter
 
 # Create your models here.
+class PetListing(models.Model):
+    AVAILABLE = 'available'
+    ADOPTED = 'adopted'
+    PENDING = 'pending'
+    STATUS_CHOICES = [
+        (AVAILABLE, 'available'),
+        (ADOPTED, 'adopted'),
+        (PENDING, 'pending'),
+    ]
+
+    FEMALE = 'female'
+    MALE = 'male'
+    UNKNOWN = 'unknown'
+    GENDER_CHOICES = [
+        (FEMALE, 'female'),
+        (MALE, 'male'),
+        (UNKNOWN, 'unknown'),
+    ]
+
+    shelter = models.ForeignKey(
+        PetUser, 
+        on_delete=models.CASCADE, 
+        related_name='pet_listings'
+        )
+    name = models.CharField(max_length=255)
+    breed = models.CharField(max_length=255)
+    age = models.PositiveIntegerField(
+        default=None,
+        null=True,
+        blank=True,
+        )
+    size = models.PositiveIntegerField(
+        default=None,
+        null=True,
+        blank=True,
+        help_text='size in cm',
+        )
+    color = models.CharField(max_length=255)
+    gender = models.CharField(
+        max_length=255,
+        choices=GENDER_CHOICES,
+        default=None,
+        null=True,
+        blank=True,
+        )
+    status = models.CharField(
+        max_length=255,
+        choices=STATUS_CHOICES,
+        default=AVAILABLE,
+        )
+    description = models.TextField(
+        default=None,
+        null=True,
+        blank=True,
+        )
+    
+    def __str__(self):
+        return 'Pet Listing: {}'.format(self.name)
