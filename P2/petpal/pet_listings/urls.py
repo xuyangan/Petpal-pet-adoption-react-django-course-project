3bedecf45b87de77c42b65e9269d11b33f275b8a
xuyangan <annaxu44@gmail.com>
2023-11-13 17:20:43 -0500

    changes the petlistings urls

--- P2/petpal/pet_listings/urls.py
+++ P2/petpal/pet_listings/urls.py
@@ -5,7 +5,7 @@ from .views import PetListingQueryList
 
 app_name = 'pet_listings'
 urlpatterns = [
-    path('user/pet_listings/', PetListingListCreate.as_view(), name='pet_listing_list'),
-    path('user/pet_listings/<int:pk>/', PetListingRetrieveUpdateDestroy.as_view(), name='pet_listing_retrieve_update_destroy'),
-    path('user/pet_listings/search/', PetListingQueryList.as_view(), name='pet_listing_retrieve_update_destroy'),
+    path('', PetListingListCreate.as_view(), name='pet_listing_list'),
+    path('<int:pk>/', PetListingRetrieveUpdateDestroy.as_view(), name='pet_listing_retrieve_update_destroy'),
+    path('search/', PetListingQueryList.as_view(), name='pet_listing_retrieve_update_destroy'),
 ]
