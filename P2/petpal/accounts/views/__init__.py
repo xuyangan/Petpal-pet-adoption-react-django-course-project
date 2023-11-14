5bea33aaff8b589cb41669d3f027e2f7b7fc6155
Emily <emilylaetitiawan@gmail.com>
2023-11-13 19:52:15 -0500

    module

--- P2/petpal/accounts/views/__init__.py
+++ P2/petpal/accounts/views/__init__.py
@@ -2,4 +2,4 @@ from .pet_users_create import PetSeekerCreate, PetShelterCreate
 from .pet_users_update import PetSeekerUpdate, PetShelterUpdate
 from .pet_users_get import PetShelterProfile, PetSeekerProfile
 from .pet_users_list import PetShelterList
-from pet_users_delete import PetShelterDestroy, PetSeekerDestroy
+from .pet_users_delete import PetShelterDestroy, PetSeekerDestroy
