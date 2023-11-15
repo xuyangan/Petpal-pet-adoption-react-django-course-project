48e6dbe4f9602b6b4bac675d9c9b95c9a352835e
Emily <emilylaetitiawan@gmail.com>
2023-11-15 10:49:39 -0500

    update

--- P2/petpal/accounts/serializers/pet_user_update_serializer.py
+++ P2/petpal/accounts/serializers/pet_user_update_serializer.py
@@ -6,7 +6,13 @@ class PetShelterUpdateSerializer(ModelSerializer):
         model = PetUser
         fields = ['email', 'password', 'shelter_name', 'phone_number', 'location', 'mission_statement', 'profile_picture']
 
+        def update(self, instance, validated_data):
+            return super().update(instance, validated_data)
+
 class PetSeekerUpdateSerializer(ModelSerializer):
     class Meta:
         model = PetUser
         fields = ['email', 'password', 'phone_number', 'location', 'preferences', 'profile_picture']
+
+        def update(self, instance, validated_data):
+            return super().update(instance, validated_data)
