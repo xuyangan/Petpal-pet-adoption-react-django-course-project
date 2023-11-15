8b5a2acc99f6d7ff694195b57f1439e513aab4c8
ZZBP <zeling.zhang@mail.utoronto.ca>
2023-11-15 11:17:48 -0500

    final part of notification

--- P2/petpal/pet_listings/serializers/pet_listing_POST_serializer.py
+++ P2/petpal/pet_listings/serializers/pet_listing_POST_serializer.py
@@ -1,6 +1,10 @@
 from .base_pet_listing_serializer import BasePetListingSerializer
 from rest_framework.serializers import ListField, ImageField
 from pet_listings.models import PetListing, PetImage
+# from accounts.models import PetUser
+# from notifications.models import Notification
+# import json
+# from django.urls import reverse
 
 class PetListingCreateSerializer(BasePetListingSerializer):
 
@@ -15,5 +19,22 @@ class PetListingCreateSerializer(BasePetListingSerializer):
             image_object = PetImage(image=image, pet_listing=pet_listing)
             image_object.save()
         
+        # self.send_notifications_to_interested_seekers(pet_listing)
+        
         return pet_listing
-    
+    # def send_notifications_to_interested_seekers(self, pet_listing):
+    #     breed = pet_listing.breed
+    #     potentially_interested_seekers = PetUser.objects.filter(preferences__isnull=False)
+    #     pet_listing_url = reverse('pet_listings:pet_listing_retrieve_update_destroy', args=[pet_listing.id])
+
+    #     for seeker in potentially_interested_seekers:
+    #         try:
+    #             if breed in json.loads(seeker.preferences):
+    #                 message = f"{pet_listing.shelter.username} has a new member {pet_listing.name}, come and check it out!"
+    #                 Notification.objects.create(
+    #                     user_id=seeker,
+    #                     message=message,
+    #                     related_link=pet_listing_url
+    #                 )
+    #         except json.JSONDecodeError:
+    #             continue
