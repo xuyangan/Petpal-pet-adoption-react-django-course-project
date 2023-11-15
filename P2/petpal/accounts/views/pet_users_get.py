7cae317c1b4e9460fa1fa2da43da3cd4520a9415
Hongzip <hongzip.kim@mail.utoronto.ca>
2023-11-15 11:38:22 -0500

    WOOHOO THIS IS FIXED

--- P2/petpal/accounts/views/pet_users_get.py
+++ P2/petpal/accounts/views/pet_users_get.py
@@ -6,8 +6,10 @@ from django.shortcuts import get_object_or_404
 from ..models import PetUser
 from accounts.serializers import PetShelterProfileSerializer, PetSeekerProfileSerializer
 
+
 class PetShelterProfile(RetrieveAPIView):
-    queryset = PetUser.objects.all().exclude(shelter_name=None).exclude(shelter_name='')
+    queryset = PetUser.objects.all().exclude(
+        shelter_name=None).exclude(shelter_name='')
     serializer_class = PetShelterProfileSerializer
     permission_classes = [IsAuthenticated]
 
@@ -16,6 +18,7 @@ class PetShelterProfile(RetrieveAPIView):
         shelter_pk = self.kwargs.get('pk')
         shelter = get_object_or_404(queryset, pk=shelter_pk)
         return shelter
+
 
 class PetSeekerProfile(RetrieveAPIView):
     serializer_class = PetSeekerProfileSerializer
@@ -26,14 +29,16 @@ class PetSeekerProfile(RetrieveAPIView):
 
         if user.is_shelter():
             # must use application queryset please check lol
-            applications = user.shelter_applications.get(pet_seeker=self.kwargs.get('pk'))
+            applications = user.shelter_applications.filter(
+                pet_seeker=self.kwargs.get('pk'))
             if applications.exists():
                 return PetUser.objects.all()
             else:
-                raise PermissionDenied("You do not have permission to view this.")
+                raise PermissionDenied(
+                    "You do not have permission to view this.")
         else:
             return PetUser.objects.all()
-    
+
     def get_object(self):
         queryset = self.filter_queryset(self.get_queryset())
         seeker_pk = self.kwargs.get('pk')
