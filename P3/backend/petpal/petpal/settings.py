263d8f3a2062349c0062eac2d35f080bcf4fe326
Emily <emilylaetitiawan@gmail.com>
2023-12-03 15:36:49 -0500

    added cors

--- P3/backend/petpal/petpal/settings.py
+++ P3/backend/petpal/petpal/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework_simplejwt',
+    'corsheaders',
     'accounts',
     'pet_listings',
     'comments',
@@ -60,6 +61,7 @@ REST_FRAMEWORK = {
 AUTH_USER_MODEL = "accounts.PetUser"
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -67,6 +69,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
 ]
 
 ROOT_URLCONF = 'petpal.urls'
