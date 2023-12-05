8d649d4364bba0f727427834af29ccf8822a4fa6
Hongzip <hongzip.kim@mail.utoronto.ca>
2023-12-04 20:46:56 -0500

    pushed some changes

--- P3/frontend/src/App.js
+++ P3/frontend/src/App.js
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import Signup from './pages/Signup';
 import SignupSeeker from './pages/SignupSeeker';
 import SignupShelter from './pages/SignupShelter';
+import ApplicationForm from './pages/ApplicationForm';
 
 function App() {
   return <BrowserRouter>
@@ -16,6 +17,7 @@ function App() {
         <Route path="signup" element={<Signup />} />
         <Route path="signup/seeker" element={<SignupSeeker />} />
         <Route path="signup/shelter" element={<SignupShelter />} />
+        <Route path="applications" element={<ApplicationForm />} />
       </Route>
     </Routes>
   </BrowserRouter>
